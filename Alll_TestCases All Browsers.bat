56c04c37873064cbed33050864461ebef3898c47
mashalenikhil-boop <mashalenikhil@gmail.com>
2026-09-25 14:50:24 +0530

    version 2

---
+++ Alll_TestCases All Browsers.bat
@@ -0,0 +1,4 @@
+pytest -v -s -n auto --html=HTMLReports/My_Report_Chrome.html --browser chromepytest -v -s -n auto --html=HTMLReport_Chrome.html --browser chrome
+pytest -v -s -n auto --html=HTMLReport/My_Report_firefox.html --browser firefox
+pytest -v -s -n auto --html=HTMLReport/My_Report_edge.html --browser edge
+pytest -v -s -n auto --html=HTMLReport/My_Report_headless.html --browser headless
